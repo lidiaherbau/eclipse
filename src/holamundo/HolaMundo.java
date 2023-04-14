ac3d1b56d9e037cbe26e3f1ae7d5b3e972e838d7
lidiaherbau <128184309+lidiaherbau@users.noreply.github.com>
2023-04-14 20:44:06 +0200

    Update HolaMundo.java

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -16,6 +16,8 @@ public class HolaMundo {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo");
 		
 	}
 
